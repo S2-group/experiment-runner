256fa894acce01fe024130a47310d8a084ce8120
nikosChalk <chalkiadakisn@gmail.com>
2022-07-01 03:33:21 +0300

    sample config fixes

--- experiment-runner/ConfigValidator/Config/RunnerConfig.py
+++ experiment-runner/ConfigValidator/Config/RunnerConfig.py
@@ -5,72 +5,81 @@ from ConfigValidator.Config.Models.FactorModel import FactorModel
 from ConfigValidator.Config.Models.RunnerContext import RunnerContext
 from ConfigValidator.Config.Models.OperationType import OperationType
 
-from typing import Dict, List
+from typing import Dict, List, Any, Optional
 from pathlib import Path
 from os.path import dirname, realpath
 
+
 class RunnerConfig:
-    # =================================================USER SPECIFIC NECESSARY CONFIG=================================================
+    ROOT_DIR = Path(dirname(realpath(__file__)))
+
+    # ================================ USER SPECIFIC NECESSARY CONFIG ================================
     # Name for this experiment
     name:                       str             = "new_runner_experiment"
+
     # Experiment operation types
     operation_type:             OperationType   = OperationType.AUTO
+
     # Run settings
     time_between_runs_in_ms:    int             = 1000
-    # Path to store results at
-    # NOTE: Path does not need to exist, will be appended with 'name' as specified in this config and created on runtime
-    results_output_path:        Path             = Path(dirname(realpath(__file__))) / 'experiments'
-    # =================================================USER SPECIFIC UNNECESSARY CONFIG===============================================
+
+    # Path to store results at. Defaults to the config file's path, inside the folder 'experiments'.
+    # NOTE: Path does not need to exist. It will be appended with the `name` as specified above and will be created on runtime
+    results_output_path:        Path             = ROOT_DIR / 'experiments'
 
     # Dynamic configurations can be one-time satisfied here before the program takes the config as-is
-    # NOTE: Setting some variable based on some criteria
+    # e.g. Setting some variable based on some criteria
     def __init__(self):
         """Executes immediately after program start, on config load"""
 
-        EventSubscriptionController.subscribe_to_multiple_events([ 
+        EventSubscriptionController.subscribe_to_multiple_events([
             (RunnerEvents.BEFORE_EXPERIMENT, self.before_experiment),
-            (RunnerEvents.BEFORE_RUN, self.before_run),
-            (RunnerEvents.START_RUN, self.start_run),
+            (RunnerEvents.BEFORE_RUN       , self.before_run       ),
+            (RunnerEvents.START_RUN        , self.start_run        ),
             (RunnerEvents.START_MEASUREMENT, self.start_measurement),
-            (RunnerEvents.INTERACT, self.interact),
-            (RunnerEvents.STOP_MEASUREMENT, self.stop_measurement),
-            (RunnerEvents.STOP_RUN, self.stop_run),
+            (RunnerEvents.INTERACT         , self.interact         ),
+            (RunnerEvents.STOP_MEASUREMENT , self.stop_measurement ),
+            (RunnerEvents.STOP_RUN         , self.stop_run         ),
             (RunnerEvents.POPULATE_RUN_DATA, self.populate_run_data),
-            (RunnerEvents.AFTER_EXPERIMENT, self.after_experiment)
+            (RunnerEvents.AFTER_EXPERIMENT , self.after_experiment )
         ])
-        
+        self.run_table = None  # Initialized later
+
         print("Custom config loaded")
 
     def create_run_table(self) -> List[Dict]:
         """Create and return the run_table here. A run_table is a List (rows) of tuples (columns), 
         representing each run performed"""
-        run_table = RunTableModel(
-            factors = [
-                FactorModel("example_factor", ['example_treatment1', 'example_treatment2'])
+        self.run_table = RunTableModel(
+            factors=[
+                FactorModel("example_factor1", ['example_treatment1', 'example_treatment2', 'example_treatment3']),
+                FactorModel("example_factor2", ['True', 'False']),
             ],
-            exclude_variations = [ #TODO: types fix
-                {"example_treatment1"},     # all runs having treatment example_treatment1 will be excluded
-                {"example_treatment1", "example_treatment2"} # all runs having the combination <treatment1, treatment2> will be excluded
-            ] 
+            exclude_variations=[
+                {"example_treatment1"},         # all runs having treatment example_treatment1 will be excluded. #FIXME: Currently, this excludes all runs having treatment 'example_treatment1' in any of the factors
+                {"example_treatment2", "True"}  # all runs having the combination <treatment1, treatment2> will be excluded #FIXME: Currently, this excludes all runs having treatment `set(treatment1, treatment2)` as a subset of all treatement combinations, in any of the factors
+            ],
+            data_columns=['avg_cpu', 'avg_mem']
         )
-        run_table.create_experiment_run_table()
-        return run_table.get_experiment_run_table()
+        return self.run_table.generate_experiment_run_table()
 
     def before_experiment(self) -> None:
-        """Perform any activity required before starting the experiment here"""
+        """Perform any activity required before starting the experiment here
+        Invoked only once during the lifetime of the program."""
 
         print("Config.before_experiment() called!")
 
     def before_run(self) -> None:
-        """Perform any activity required before starting a run, no context is available 
-        here as the run is not yet active (BEFORE RUN)"""
+        """Perform any activity required before starting a run.
+        No context is available here as the run is not yet active (BEFORE RUN)"""
 
         print("Config.before_run() called!")
 
     def start_run(self, context: RunnerContext) -> None:
-        """Perform any activity required for starting the run here. 
-        Activities before and after starting the run should also be performed here."""
-        
+        """Perform any activity required for starting the run here.
+        For example, starting the target system to measure.
+        Activities after starting the run should also be performed here."""
+
         print("Config.start_run() called!")
 
     def start_measurement(self, context: RunnerContext) -> None:
@@ -78,31 +87,34 @@ class RunnerConfig:
         print("Config.start_measurement() called!")
 
     def interact(self, context: RunnerContext) -> None:
-        """Perform any interaction with the running system in question here."""
+        """Perform any interaction with the running target system here."""
 
         print("Config.interact() called!")
 
     def stop_measurement(self, context: RunnerContext) -> None:
         """Perform any activity here required for stopping measurements."""
+
         print("Config.stop_measurement called!")
 
     def stop_run(self, context: RunnerContext) -> None:
-        """Perform any activity required for stopping the run here.
-        Activities before and after stopping the run should also be performed here."""
-        
+        """Perform any activity here required for stopping the run.
+        Activities after stopping the run should also be performed here."""
+
         print("Config.stop_run() called!")
-    
-    def populate_run_data(self, context: RunnerContext) -> tuple:
-        """Return the run data as a row for the output manager represented as a tuple"""
+
+    def populate_run_data(self, context: RunnerContext) -> Optional[Dict[str, Any]]:
+        """Parse and process any measurement data here.
+        You can also store the raw measurement data under `context.run_dir`
+        Returns a dictionary with keys `self.run_table.data_columns` and their values populated"""
 
         print("Config.populate_run_data() called!")
         return None
 
     def after_experiment(self) -> None:
-        """Perform any activity required after stopping the experiment here"""
+        """Perform any activity required after stopping the experiment here
+        Invoked only once during the lifetime of the program."""
 
         print("Config.after_experiment() called!")
 
-    # ===============================================DO NOT ALTER BELOW THIS LINE=================================================
-    # NOTE: Do not alter these values
+    # ================================ DO NOT ALTER BELOW THIS LINE ================================
     experiment_path:            Path             = None
