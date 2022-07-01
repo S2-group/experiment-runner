17229faff6dddbc37bf3ba7c72ce143424bd9b4f
nikosChalk <chalkiadakisn@gmail.com>
2022-07-01 03:07:56 +0300

    removed unecessary state

--- experiment-runner/ConfigValidator/Config/Models/RunTableModel.py
+++ experiment-runner/ConfigValidator/Config/Models/RunTableModel.py
@@ -5,27 +5,29 @@ from ConfigValidator.Config.Models.FactorModel import FactorModel
 
 class RunTableModel:
     __factors:       List[FactorModel] = None
-    __experiment_run_table:          List[Dict]  = None
     __exclude_variations:            List[Dict]  = None
     __data_columns:                  List[str]   = None
 
     def __init__(self, factors: List[FactorModel], exclude_variations: List[Dict] = None, data_columns: List[str] = None):
         self.__factors = factors
-        self.__experiment_run_table = []
         self.__exclude_variations = exclude_variations
         self.__data_columns = data_columns
 
     def get_factors(self) -> List[FactorModel]:
         return self.__factors
 
-    def get_experiment_run_table(self) -> List[Dict]:
-        return self.__experiment_run_table
+    def get_data_columns(self) -> List[str]:
+        return self.__data_columns
 
-    def create_experiment_run_table(self) -> None:
+    def generate_experiment_run_table(self) -> List[Dict]:
         def __filter_list(filter_list: List[Tuple]):
             if self.__exclude_variations is None:
                 return filter_list
 
+            #FIXME: we should use dictionaries instead of sets
+            #For example, imagine that you have 2 numerical factors in range [0,100] and you want to skip combination (factor1=20, factor2=50)
+            #With the current setupt, you will end up skipping (factor1=20, factor2=50) AND (factor2=20, factor1=50)
+            #The user, as a temporary workaround, can prefix their factor levels to distinguish them.
             for exclusion in self.__exclude_variations:
                 filter_list = [x for x in filter_list if not exclusion <= set(x)]
 
@@ -46,6 +48,7 @@ class RunTableModel:
             for data_column in self.__data_columns:
                 column_names.append(data_column)
 
+        experiment_run_table = []
         for i in range(0, len(filtered_list)):
             row_list = list(filtered_list[i])
             row_list.insert(0, f'run_{i}')         # __run_id
@@ -55,4 +58,5 @@ class RunTableModel:
                 for data_column in self.__data_columns:
                     row_list.append(" ")
 
-            self.__experiment_run_table.append(dict(zip(column_names, row_list)))
+            experiment_run_table.append(dict(zip(column_names, row_list)))
+        return experiment_run_table
