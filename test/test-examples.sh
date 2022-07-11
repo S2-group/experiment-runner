f54a973feada9a8266ea6d70c9390b22bd6af9f0
nikosChalk <chalkiadakisn@gmail.com>
2022-07-11 22:17:52 +0300

    pycharm hack

--- test/test-examples.sh
+++ test/test-examples.sh
@@ -3,6 +3,15 @@
 export PYTHONUNBUFFERED=1
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 
+if ! command -v python &> /dev/null
+then
+    echo "Script ran from PyCharm sh shell. Setting python"
+    # hacky way for `alias` in sh
+    python () {
+        ${PROJECT_DIR}/python "$@"
+    }
+fi
+
 set -e
 examples=()
 for directory in examples/*/ ; do
