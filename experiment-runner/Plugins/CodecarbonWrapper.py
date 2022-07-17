ada902a8ef69bda4983a2c1b71d8b59faa8c368d
nikosChalk <chalkiadakisn@gmail.com>
2022-07-17 22:52:58 +0300

    codecarbon plugin

---
+++ experiment-runner/Plugins/CodecarbonWrapper.py
@@ -0,0 +1,66 @@
+############################################### codecarbon plugin ###############################################
+#
+# pip install codecarbon
+# Relevant issues:
+#  * "AttributeError: Jumbotron was deprecated in dash-bootstrap-components version 1.0.0." - https://github.com/mlco2/codecarbon/issues/319
+#  * "Unable to read Intel RAPL files for CPU power : Permission denied" https://github.com/mlco2/codecarbon/issues/244
+# To find country codes, use the ISO 3166-1 Alpha-3 code from https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+#
+#################################################################################################################
+
+import codecarbon
+
+from ConfigValidator.Config.Models.RunnerContext import RunnerContext
+from ConfigValidator.Config.RunnerConfig import RunnerConfig
+
+# def emission_tracker(*decargs, **deckwargs):
+#     def emission_tracker_decorator(cls):
+#         cls.start_measurement = start_emission_tracker(*decargs, **deckwargs)(cls.start_measurement)
+#         return cls
+#     return emission_tracker_decorator
+
+def start_emission_tracker(online=False, *decargs, **deckwargs):
+    def start_emission_tracker_decorator(func):
+        def wrapper(*args, **kwargs):
+            self: RunnerConfig = args[0]
+            context: RunnerContext = args[1]
+
+            if 'project_name' not in deckwargs:
+                deckwargs['project_name'] = self.name
+            if 'output_dir' not in deckwargs:
+                deckwargs['output_dir'] = str(context.run_dir.resolve())
+            codecarbon_cls = codecarbon.EmissionsTracker if online else codecarbon.OfflineEmissionsTracker
+
+            self.__emission_tracker__ = codecarbon_cls(*decargs, **deckwargs)
+            self.__emission_tracker__.start()
+            return func(*args, **kwargs)
+        return wrapper
+    return start_emission_tracker_decorator
+
+def stop_emission_tracker(func):
+    def wrapper(*args, **kwargs):
+        self: RunnerConfig = args[0]
+
+        ret_val = func(*args, **kwargs)
+        self.__emissions__ = self.__emission_tracker__.stop()  # store it for usage later
+        return ret_val
+    return wrapper
+
+def add_co2_data_column(func):
+    def wrapper(*args, **kwargs):
+        self: RunnerConfig = args[0]
+
+        func(*args, **kwargs)  # will set self.run_table_model. Discard result
+        self.run_table_model.get_data_columns().insert(2, '__co2_emissions')
+        return self.run_table_model.generate_experiment_run_table()  # FIXME: this is bad
+    return wrapper
+
+def populate_co2_data(func):
+    def wrapper(*args, **kwargs):
+        self: RunnerConfig = args[0]
+
+        ret_val = func(*args, **kwargs)
+        ret_val['__co2_emissions'] = self.__emissions__
+
+        return ret_val
+    return wrapper
---
+++ test/Plugins/test_CodecarbonWrapper.py
@@ -0,0 +1,71 @@
+import unittest
+
+import shutil
+import tempfile
+import re
+from pathlib import Path
+from typing import AnyStr
+
+from ConfigValidator.Config.Models.RunnerContext import RunnerContext
+from ConfigValidator.Config.RunnerConfig import RunnerConfig
+from ProgressManager.Output.OutputProcedure import OutputProcedure as output
+
+from Plugins import CodecarbonWrapper
+
+
+class TestEmissionTrackerIndividual(unittest.TestCase):
+
+    class EmissionTrackerConfig(RunnerConfig):
+        # setUpClass and tearDownClass appear broken *sigh*. Use this wrapper instead. We have a singleton anyway.
+        tmpdir: AnyStr = tempfile.mkdtemp()
+        def clear(self):
+            print(f'Clearing tmpdir {self.__class__.tmpdir}')
+            shutil.rmtree(self.__class__.tmpdir)
+
+        @CodecarbonWrapper.add_co2_data_column
+        def create_run_table(self):
+            super().create_run_table()
+
+        @CodecarbonWrapper.start_emission_tracker(
+            country_iso_code="NLD",
+            output_dir=tmpdir
+        )
+        def start_measurement(self, context: RunnerContext):
+            super().start_measurement(context)
+
+        def interact(self, context: RunnerContext):
+            output.console_log("Config.interact() called!")
+            re.search(r'^(a|a?)+b$', "a" * 20)  # ReDoS to consume some cpu
+
+        @CodecarbonWrapper.stop_emission_tracker
+        def stop_measurement(self, context: RunnerContext):
+            super().stop_measurement(context)
+
+        @CodecarbonWrapper.populate_co2_data
+        def populate_run_data(self, context: RunnerContext):
+            output.console_log("Config.populate_run_data() called!")
+            return {
+                'avg_cpu': 52.3,
+                'avg_mem': 18.1
+            }
+
+    def setUp(self) -> None:
+        self.runner_config = self.__class__.EmissionTrackerConfig()
+        self.run_table = self.runner_config.create_run_table()
+
+    def tearDown(self) -> None:
+        self.runner_config.clear()
+
+    def test_config(self):
+        self.runner_config.start_measurement(None)
+        self.runner_config.interact(None)
+        self.runner_config.stop_measurement(None)
+        run_data = self.runner_config.populate_run_data(None)
+        self.assertIn('__co2_emissions', run_data)
+        self.assertTrue(run_data['__co2_emissions'] > 0)
+        self.assertTrue( (Path(self.runner_config.tmpdir) / 'emissions.csv').is_file() )
+        print(run_data)
+
+
+if __name__ == '__main__':
+    unittest.main()
