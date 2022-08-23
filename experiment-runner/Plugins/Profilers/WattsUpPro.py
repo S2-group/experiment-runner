9465df9c9a86293a2ae3d34d6d8ea29a210aa28f
nikosChalk <chalkiadakisn@gmail.com>
2022-08-23 19:35:15 +0300

    WattsUpPro fixes & doc

--- experiment-runner/Plugins/Profilers/WattsUpPro.py
+++ experiment-runner/Plugins/Profilers/WattsUpPro.py
@@ -1,16 +1,31 @@
 import os, serial
 import datetime, time
-import argparse
-import curses
 from platform import uname
 
-EXTERNAL_MODE = 'E'
-INTERNAL_MODE = 'I'
-TCPIP_MODE = 'T'
-FULLHANDLING = 2
+class WattsUpPro(object):
+    """An integration of "Watts up? Pro" power meter: https://github.com/isaaclino/wattsup"""
+    EXTERNAL_MODE = 'E'
+    INTERNAL_MODE = 'I'
+    TCPIP_MODE = 'T'
+    FULLHANDLING = 2
 
-class WattsUp(object):
-    def __init__(self, port, interval):
+    def __init__(self, port: str = None, interval=1.0):
+
+        # Set up & check serial ports
+        if port is None:
+            system = uname()[0]
+            if system == 'Darwin':          # OS X
+                port = '/dev/tty.usbserial-A1000wT3'
+            elif system == 'Linux':
+                port = '/dev/ttyUSB0'
+
+        if not os.path.exists(port):
+                print( '')
+                print( 'Serial port %s does not exist.' % port)
+                print( 'Please make sure FTDI drivers are installed')
+                print( ' (http://www.ftdichip.com/Drivers/VCP.htm)')
+                print( 'Default port is /dev/ttyUSB0 for Linux')
+                raise RuntimeError("Invalid port")
 
         self.s = serial.Serial(port, 115200 )
         self.logfile = None
@@ -24,12 +39,12 @@ class WattsUp(object):
     def mode(self, runmode):
         temp = '#L,W,3,%s,,%d;' % (runmode, self.interval)
         self.s.write( str.encode(temp))
-        if runmode == INTERNAL_MODE:
-            self.s.write('#O,W,1,%d' % FULLHANDLING)
+        if runmode == self.INTERNAL_MODE:
+            self.s.write('#O,W,1,%d' % self.FULLHANDLING)
 
     def log(self,timeout, logfile = None):
         print('Logging...')
-        self.mode(EXTERNAL_MODE)
+        self.mode(self.EXTERNAL_MODE)
         if logfile:
             self.logfile = logfile
             o = open(self.logfile,'w')
@@ -43,9 +58,9 @@ class WattsUp(object):
             if line.startswith( str.encode('#d') ):
                 fields = line.split(str.encode(','))
                 if len(fields)>5:
-                    W = float(fields[3]) / 10;
-                    V = float(fields[4]) / 10;
-                    A = float(fields[5]) / 1000;
+                    W = float(fields[3]) / 10
+                    V = float(fields[4]) / 10
+                    A = float(fields[5]) / 1000
                    
                     if self.logfile:
                         o.write('%s %d %3.1f %3.1f %5.3f\n' % (datetime.datetime.now(), n, W, V, A))  # SAVE TO LOG
@@ -56,27 +71,3 @@ class WattsUp(object):
             o.close()
         except:
             pass
-
-    def sysCheck(self, port, timeout, outfile):           # Set up & check serial ports 
-        if not port:
-            system = uname()[0]
-            if system == 'Darwin':          # OS X
-                port = '/dev/tty.usbserial-A1000wT3'
-            elif system == 'Linux':
-                port = '/dev/ttyUSB0'
-
-        if not self.interval:
-            self.interval = 1.0
-        if not os.path.exists(port):
-                print( '')
-                print( 'Serial port %s does not exist.' % port)
-                print( 'Please make sure FDTI drivers are installed')
-                print( ' (http://www.ftdichip.com/Drivers/VCP.htm)')
-                print( 'Default port is /dev/ttyUSB0 for Linux')
-                exit()
-        self.log(timeout, outfile)
-
-def run_meter(port, interval, timeout, outfile):
-    meter = WattsUp(port, interval)            # init (Serial port, interval) *ttyUSB0 Linux default
-    meter.sysCheck(port, timeout, outfile)     # run experiment (Serial port, timeout, export file)
-    
